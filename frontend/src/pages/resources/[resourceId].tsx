cbc615c9f7f8fd2296ce9ff7b0ced9c189368f98
saloni kumawat <545saloni@gmail.com>
2022-06-27 00:16:27 +0200

    Fetching user name and device name

--- frontend/src/pages/resources/[resourceId].tsx
+++ frontend/src/pages/resources/[resourceId].tsx
@@ -6,19 +6,29 @@ import { useEffect, useState } from 'react';
 
 
 const Resource : NextPage = () => {
-    const [userId, setUserId] = useState("");
-    const [deviceId, setDeviceId] = useState("");
+    const [userName, setUserName] = useState("");
+    const [deviceName, setDeviceName] = useState("");
 
     const router = useRouter();
     const { resourceId } = router.query;
 
     const fetchResource = async () => { 
         if (resourceId) {
+            // Fetch resource.
             let fullUrl = "http://localhost:3001/resources/" + resourceId;
             const response = await fetch(fullUrl, {method: "GET"});
             const fetchedResource = await response.json();
-            setUserId(fetchedResource.user_id);
-            setDeviceId(fetchedResource.device_id);
+
+            // Fetch user name.
+            const responseUser = await fetch("http://localhost:3001/users/" + fetchedResource.user_id, {method: "GET"});
+            const fetchedUser = await responseUser.json();
+            setUserName(fetchedUser.name);
+
+
+            // Fetch device name.
+            const responseDevice = await fetch("http://localhost:3001/devices/" + fetchedResource.device_id, {method: "GET"});
+            const fetchedDevice = await responseDevice.json();
+            setDeviceName(fetchedDevice.name);
         }
     }
 
@@ -27,8 +37,8 @@ const Resource : NextPage = () => {
     return (
         <div>
             <h1>Hello! This is resource: { resourceId } page.</h1>
-            <p>User Id: {userId}</p>
-            <p>Device Id: {deviceId}</p>
+            <p>User Name: {userName}</p>
+            <p>Device Name: {deviceName}</p>
         </div>
         
     ); 
