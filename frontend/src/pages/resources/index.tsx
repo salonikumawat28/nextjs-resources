1ca160fb86b55bebc0cd44b3865c1dde6d74f93d
saloni kumawat <545saloni@gmail.com>
2022-06-26 18:15:03 +0200

    Fetching Resources from backend server

--- frontend/src/pages/resources/index.tsx
+++ frontend/src/pages/resources/index.tsx
@@ -1,36 +1,26 @@
 import type { NextPage } from 'next';
-
-const resources = {
-    1: {
-      id: 1,
-      user_id: 1,
-      device_id: 1,
-    },
-    2: {
-      id: 2,
-      user_id: 2,
-      device_id: 2,
-    },
-    3: {
-      id: 3,
-      user_id: 3,
-      device_id: 3,
-    },
-  };
+import { useEffect, useState } from 'react';
 
 const Resources: NextPage = () => {
+    const [resources, setResources] = useState({});
+
+    const fetchResources = async () => {
+        const response = await fetch("http://localhost:3001/resources", {method: 'GET'});
+        const fetchedResources = await response.json();
+        setResources(fetchedResources); 
+    }
+
+    useEffect(() => { fetchResources() }, []);
+
     return (
         <div>
             <h1>Resources List</h1>
             <div>
                 <ul>
-                    {Object.keys(resources).map(resource => <li>Resource {resource}</li>)}
-                    
+                    {Object.keys(resources).map(resource => <li key={resource}>Resource: {resource}</li>)}
                 </ul>
             </div>
         </div>
-        
-
     ); 
 };
 
