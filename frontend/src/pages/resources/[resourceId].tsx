daf20cc88b2a98bcf9b68a4103df9870a74bebd2
saloni kumawat <545saloni@gmail.com>
2022-06-27 00:37:51 +0200

    Process response data only if 200 status

--- frontend/src/pages/resources/[resourceId].tsx
+++ frontend/src/pages/resources/[resourceId].tsx
@@ -17,18 +17,23 @@ const Resource : NextPage = () => {
             // Fetch resource.
             let fullUrl = "http://localhost:3001/resources/" + resourceId;
             const response = await fetch(fullUrl, {method: "GET"});
+            if (response.status !== 200) return;
             const fetchedResource = await response.json();
 
             // Fetch user name.
             const responseUser = await fetch("http://localhost:3001/users/" + fetchedResource.user_id, {method: "GET"});
-            const fetchedUser = await responseUser.json();
-            setUserName(fetchedUser.name);
-
+            if (responseUser.status === 200) {
+                const fetchedUser = await responseUser.json();
+                setUserName(fetchedUser.name);
+            }
+            
 
             // Fetch device name.
             const responseDevice = await fetch("http://localhost:3001/devices/" + fetchedResource.device_id, {method: "GET"});
-            const fetchedDevice = await responseDevice.json();
-            setDeviceName(fetchedDevice.name);
+            if (responseDevice.status === 200){
+                const fetchedDevice = await responseDevice.json();
+                setDeviceName(fetchedDevice.name);
+            }
         }
     }
 
@@ -37,8 +42,8 @@ const Resource : NextPage = () => {
     return (
         <div>
             <h1>Hello! This is resource: { resourceId } page.</h1>
-            <p>User Name: {userName}</p>
-            <p>Device Name: {deviceName}</p>
+            {userName && <p>User Name: {userName}</p>}
+            {deviceName && <p>Device Name: {deviceName}</p>}
         </div>
         
     ); 
